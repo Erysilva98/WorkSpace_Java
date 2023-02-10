267830d234b93c12636a65cd66534dac38549719
Ery.Java <erimilsonsilva98@gmail.com>
2023-02-10 10:21:36 -0300

    aula 10/02

---
+++ Aula.10-02/MyfirstApp.java
@@ -0,0 +1,6 @@
+public class MyfirstApp{
+	public static void main (String[]  Args){
+		System.out.print("I Rule");
+		System.out.print("The World");
+	}
+}
---
+++ MyfirstApp/src/MyfirstApp.java
@@ -0,0 +1,10 @@
+
+public class MyfirstApp {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+		System.out.println("Hello Word!");
+		
+	}
+
+}
