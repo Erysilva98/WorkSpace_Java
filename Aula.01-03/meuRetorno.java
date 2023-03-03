b5558fc069a463898825341db433ee3358fc8f6f
EryJava <erimilson1998@gmail.com>
2023-03-03 08:14:02 -0300

    aula 01-03

---
+++ Aula.01-03/meuRetorno.java
@@ -0,0 +1,13 @@
+public class meuRetorno {
+
+    MeuRetorno giveSecret()
+    {
+        MeuRetorno retorno = mew 
+    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+		
+	}
+
+}
